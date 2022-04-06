e7472b5431b028cc287fc9ff1276adbd7dc7651b
ajt <alexjeffcott@gmail.com>
2022-04-06 22:10:55 +0100

    feat: remove annotate package

--- packages/frontend-react/src/routes/Router.tsx
+++ packages/frontend-react/src/routes/Router.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ErrorBoundary, LazyLoader } from '../components';
 
 const LandingPage = lazy(() => import('./landingPage/LandingPage'));
-const AnnotationDemoPage = lazy(() => import('./annotationDemoPage/AnnotationDemoPage'));
 
 const Router = () => (
 	<ErrorBoundary>
@@ -11,7 +10,6 @@ const Router = () => (
 			<Suspense fallback={<LazyLoader />}>
 				<Routes>
 					<Route path="/" element={<LandingPage />} />
-					<Route path="/annotate" element={<AnnotationDemoPage />} />
 				</Routes>
 			</Suspense>
 		</BrowserRouter>
